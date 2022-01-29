6a5700e8f39843772a3bbcf7b123d53d7b7a962d
114514dewa <98639645+114514dewa@users.noreply.github.com>
2022-01-29 17:04:48 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 5) return '希哥给您拜年了';
+        if (cps <= 8) return '得得得得得';
+        if (cps <= 10)  return '新年快乐';
+        if (cps <= 15) return '来根华子';
+        return '祝您幸福安康？';
     }
 
     function toStr(obj) {
